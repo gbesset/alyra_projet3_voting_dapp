da9d6d7d4a0d992f17969e9628f2fddb4dcd2dd9
gbesset <guillaume.besset.pro@gmail.com>
2023-03-07 18:26:59 +0100

    Fix: simpleStorage ok

--- client/src/pages/protected/SimpleStorage.jsx
+++ client/src/pages/protected/SimpleStorage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+import Intro from "../../components/Intro/";
+import Setup from "../../components/Setup";
+import {Demo} from "../../components/Demo";
+import Footer from "../../components/Footer";
 
 export const SimpleStorage = () => {
     return (
