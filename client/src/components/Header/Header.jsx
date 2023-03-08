6faa891458874a4b1455106787ff66e971fc7b59
gbesset <guillaume.besset.pro@gmail.com>
2023-03-08 11:58:48 +0100

    Feat: landing page initialisation

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,43 +3,43 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return (
+        <header>
+            <nav className="navbar is-spaced" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <Link className="navbar-item" to="/">
+                    <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/sites/2147485225/images/a4df30a-cf3c-ee5a-b52a-4d6484824c0b_logo_bleu_2000x.png" width="112" height="28" />
+                </Link>
 
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <Link className="navbar-item" to="/">
-                <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/sites/2147485225/images/a4df30a-cf3c-ee5a-b52a-4d6484824c0b_logo_bleu_2000x.png" width="112" height="28" />
-            </Link>
-
-            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-start">
-                <Link to="/home" className="navbar-item">Home</Link>
-                <Link to="/protected/voter" className="navbar-item">Voter</Link>
-                <Link to="/protected/admin" className="navbar-item">Admin</Link>
-                <Link to="/protected/simpleStorage" className="navbar-item">SimpleStorage</Link>
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
-        </div>
 
-        <div className="navbar-end">
-            <div className="navbar-item">
-                <div className="buttons">
-                    <a className="button is-primary">
-                    <strong>Sign up</strong>
-                    </a>
-                    <a className="button is-light">
-                    Log in
-                    </a>
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <Link to="/home" className="navbar-item">Home</Link>
+                    <Link to="/protected/voter" className="navbar-item">Voter</Link>
+                    <Link to="/protected/admin" className="navbar-item">Admin</Link>
+                    <Link to="/protected/simpleStorage" className="navbar-item">SimpleStorage</Link>
                 </div>
             </div>
-        </div>
-    </nav>
-            
+
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <div className="buttons">
+                        <a className="button is-primary">
+                        <strong>Sign up</strong>
+                        </a>
+                        <a className="button is-light">
+                        Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
         
     );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,40 @@ import React from 'react';
 
 export const Home = () => {
     return (
-        <div class="content">
-            <h1>Projet Voting</h1>
-            <p>...</p>
-        </div>
+        <>
+            <section className="section">
+            <div className="container">
+            <div className="columns">
+                <div className="column">
+                <img src="/images/voting.jpg" alt="background" />
+                </div>
+                <div className="column">
+                <div className="is-flex is-flex-direction-column is-justify-content-center h-100">
+                <h1 className="title">Projet #3 Dapp Voting</h1>
+                <p className="subtitle pt-2">Developed by:
+                    <ul className="pl-2">
+                        <li className="pt-2">Guillaume Besset</li>
+                        <li className="pt-2">Eli Gonzalez Zarate</li>
+                    </ul> 
+                </p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        <section className="section">
+            <div className="container has-text-centered">
+            <h1 className="title">How to do ?</h1>
+            <div>
+                <ul className="is-inline-block has-text-left">
+                    <li>Connect with your Wallet</li>
+                    <li>Be white-listed</li>
+                    <li>Make some proposals and vote</li>
+                </ul>
+            </div>
+            <button className="button is-primary mt-2">Go !</button>
+            </div>
+        </section>
+      </>
     );
 };
