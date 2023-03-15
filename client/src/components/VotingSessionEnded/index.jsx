db3e4be69edb1da7425e89b55e7474e9e6afdcc4
gbesset <guillaume.besset.pro@gmail.com>
2023-03-15 15:31:26 +0100

    CLEAN: vote

--- client/src/components/VotingSessionEnded/index.jsx
+++ client/src/components/VotingSessionEnded/index.jsx
@@ -4,25 +4,34 @@ import {WORKFLOW_STATUS} from '../../utils/utils.js'
 import { VoteList } from '../VotingSessionStarted/VoteList';
 
 export const VotingSessionEnded = ({upgradeWorkflowStatus}) => {
-    const { state: { contract, accounts, artifact, isOwner} } = useEth();
+    const { state: { contract, accounts, artifact, isOwner, isVoter} } = useEth();
 
 
-    function handleStatusChange(){
+    async function handleStatusChange(){
+        if(contract){
+            await contract.methods.tallyVotes().send({from:accounts[0]})
+        }
         upgradeWorkflowStatus(WORKFLOW_STATUS.VotesTallied);
     }
 
     return (
-        <div className="debug">
+        <div >
            
             <>
+            {isVoter ?(
+                <>
                 <div className="is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center">
                     <h1 className='title'>VotingSessionEnded</h1>
-                    { isOwner ? ( <button className="button is-primary" onClick={handleStatusChange}>Change state</button>) : ('')}
+                    { isOwner ? ( <button className="button is-primary" onClick={handleStatusChange}>Tally votes</button>) : ('')}
                 </div>
 
                <h2 className="subtitle">Wait for the results.....</h2>
                <VoteList />
-               
+               </>
+            ) : (
+                <p>You are not whitelisted and can't access to that data.</p>  
+
+            )}
             </>
             
             </div>
