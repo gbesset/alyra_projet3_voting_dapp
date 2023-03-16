9b603c63890f86125be51be5f29a9499b9c0719e
gbesset <guillaume.besset.pro@gmail.com>
2023-03-16 15:29:26 +0100

    FEATDoS gas limit fix

--- truffle/contracts/Voting.sol
+++ truffle/contracts/Voting.sol
@@ -91,6 +91,11 @@ contract Voting is Ownable {
         voters[msg.sender].hasVoted = true;
         proposalsArray[_id].voteCount++;
 
+        //Temporarty Winner (for DoS gas Limit)
+        if(proposalsArray[_id].voteCount > proposalsArray[winningProposalID].voteCount){
+            winningProposalID = _id;
+        }
+
         emit Voted(msg.sender, _id);
     }
 
@@ -128,15 +133,7 @@ contract Voting is Ownable {
 
 
    function tallyVotes() external onlyOwner {
-       require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
-       uint _winningProposalId;
-      for (uint256 p = 0; p < proposalsArray.length; p++) {
-           if (proposalsArray[p].voteCount > proposalsArray[_winningProposalId].voteCount) {
-               _winningProposalId = p;
-          }
-       }
-       winningProposalID = _winningProposalId;
-       
+       require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");       
        workflowStatus = WorkflowStatus.VotesTallied;
        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
     }
